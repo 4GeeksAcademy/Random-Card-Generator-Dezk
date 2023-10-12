bf0bfa443214b1a4342cf275a33ab1b8112fcce5
Rodrigo Barrera <138847032+DezkDev@users.noreply.github.com>
2023-10-12 20:24:03 +0000

    Random Card Generator finalizado

--- src/app.js
+++ src/app.js
@@ -1,21 +1,46 @@
 /* eslint-disable */
 import "bootstrap";
+import "./style.css";
 
-let pronoun = ["el", "un", "algun", "otro"];
-let adj = ["gran", "super", "genial"];
-let noun = ["futbolista", "boxeador", "programador", "doctor", "constructor"];
-let domain = [".com", ".net", ".org", ".info", ".es", ".cl"];
+const cardNumber = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+const cardSymbol = ["♥", "♦️", "♣", "♠"];
 
-function print() {
-    for (let i = 0; i < pronoun.length; i++) {
-        for (let j = 0; j < adj.length; j++) {
-            for (let k = 0; k < noun.length; k++) {
-                for (let l = 0; l < domain.length; l++) {
-                console.log(`${pronoun[i]}${adj[j]}${noun[k]}${domain[l]}`)
-                }
-            }
-        }
-    }
+function randomNumber(parameterNumber) {
+  let azar = Math.floor(Math.random() * parameterNumber.length);
+  let elementNumber = document.getElementById("numero");
+  elementNumber.innerHTML = parameterNumber[azar];
+  return cardNumber[azar];
 }
 
-print();
+function randomSymbol(parameterSymbol) {
+  let azar = Math.floor(Math.random() * parameterSymbol.length);
+  let elementSymbol = document.getElementsByClassName("simbolo");
+
+  for (let i = 0; i < elementSymbol.length; i++) {
+    elementSymbol[i].innerHTML = parameterSymbol[azar];
+    if (parameterSymbol[azar] === "♥" || parameterSymbol[azar] === "♦️") {
+      elementSymbol[i].classList.add("red");
+    }
+  }
+
+  return cardSymbol[azar];
+}
+
+window.onload = function() {
+  randomNumber(cardNumber);
+  randomSymbol(cardSymbol);
+};
