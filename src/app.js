c0f9b7ae7e263e113d008f27d76a30ffd9a89346
Rodrigo Barrera <138847032+DezkDev@users.noreply.github.com>
2023-10-12 13:04:51 +0000

    Cambie la impresion aleatoria, por una impresion de todas las posibles combinaciones

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 /* eslint-disable */
 import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronoun = ["el", "un", "algun", "otro"];
+let adj = ["gran", "super", "genial"];
+let noun = ["futbolista", "boxeador", "programador", "doctor", "constructor"];
+let domain = [".com", ".net", ".org", ".info", ".es", ".cl"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function print() {
+    for (let i = 0; i < pronoun.length; i++) {
+        for (let j = 0; j < adj.length; j++) {
+            for (let k = 0; k < noun.length; k++) {
+                for (let l = 0; l < domain.length; l++) {
+                console.log(`${pronoun[i]}${adj[j]}${noun[k]}${domain[l]}`)
+                }
+            }
+        }
+    }
+}
+
+print();
